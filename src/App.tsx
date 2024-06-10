849a0df8c33eaa08d41213a5edcfc95a52cd2c98
s.kekedzhan <s.kekedzhan@fingineers.ru>
2024-06-10 16:45:14 +0300

    react router dom and async Suspense components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import Counter from "./components/Counter";
+import  { Suspense } from 'react';
 import "./index.scss"
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MianPage/MainPage.async";
 
 const App = () => {
     return (
         <div className={"app"}>
-            Test
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={"...Loading"}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
