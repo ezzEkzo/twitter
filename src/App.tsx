5cf4aaed60af9ffe860b633a62d196f89c01353f
s.kekedzhan <s.kekedzhan@fingineers.ru>
2024-06-10 18:05:56 +0300

    global styles, and Theme Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import  { Suspense } from 'react';
-import "./index.scss"
+import "./styles/index.scss"
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MianPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className={"app"}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={"...Loading"}>
