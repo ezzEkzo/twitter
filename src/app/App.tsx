d581d51d05e68823873da062d7deda82f2471129
s.kekedzhan <s.kekedzhan@fingineers.ru>
2024-06-13 18:32:00 +0300

    routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,9 @@
 import React, {useContext, useState} from 'react';
-import  { Suspense } from 'react';
 import "./styles/index.scss"
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "pages/AboutPage/ui/AboutPage.async";
-import {MainPageAsync} from "pages/MianPage/ui/MainPage.async";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MianPage";
-
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -20,12 +15,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={"...Loading"}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
