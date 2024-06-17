ef7b16037f9696e414015f2ea9ce103485f0a72c
s.kekedzhan <s.kekedzhan@fingineers.ru>
2024-06-17 17:33:48 +0300

    add translate for link

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from "./Navbar.module.scss"
 import AppLink, {AppLinkTheme} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {useTranslation} from "react-i18next";
 
 interface NavbarProps {
     className?: string;
 }
 
 const Navbar = ({className}: NavbarProps) => {
+    const { t, i18n } = useTranslation()
+
     return (
         <div className={classNames(styles.Navbar, {}, [className])}>
             <div className={classNames(styles.links)}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={classNames(styles.mainLink)}>Главная</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={classNames(styles.mainLink)}>{t("Главная страница")}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>{t("О сайте")}</AppLink>
             </div>
         </div>
     );
